660c673c42ac94d05ba5ead21adc43a20a338aa3
Wei Bai <baiweiyj@qq.com>
2017-04-26 14:13:24 +0100

    Rotate array: three step rotate.

---
+++ src/ArrayGroup/RotateArray.java
@@ -0,0 +1,43 @@
+package ArrayGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by baiwe on 2017/4/26.
+ */
+public class RotateArray {
+
+    public static void main(String[] args){
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+
+
+        RotateArray ra = new RotateArray();
+        ra.rotate(arr, 2);
+
+        for (int num : arr){
+            System.out.println(num);
+        }
+    }
+
+    public void rotate(ArrayList<Integer> arr, int p){
+
+            reverse(arr, 0, p - 1);
+            reverse(arr, p, arr.size() - 1);
+            reverse(arr, 0, arr.size() - 1);
+
+    }
+
+    public void reverse(ArrayList<Integer> arr, int start, int end){
+        for (int i = start, j = end; i < j; i++, j--){
+            int tmp = arr.get(i);
+            arr.set(i,arr.get(j));
+            arr.set(j,tmp);
+        }
+    }
+}
