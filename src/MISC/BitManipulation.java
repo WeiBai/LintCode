7a1adec50c1af09fb2b8d29dd45f93a1f03edd81
Wei Bai <baiweiyj@qq.com>
2017-05-30 16:43:38 +0100

    Bit manipulation.

---
+++ src/MISC/BitManipulation.java
@@ -0,0 +1,13 @@
+package MISC;
+
+/**
+ * Created by baiwe on 2017/5/30.
+ */
+public class BitManipulation {
+
+    public static void main(String[] args){
+        int a = 2;
+        int b = 3;
+        System.out.println(a ^ b);
+    }
+}
